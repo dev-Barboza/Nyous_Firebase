218d104ac576841c86a1c3d1097910601732d671
Matheus Henrique <matheus.barboza.henrique@gmail.com>
2020-12-11 10:09:34 -0300

    Aplicando Autenticação

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB0EO_uVWoWOXQihRwfcMv9UN2au-xZB7A",
+    authDomain: "nyous-4c48f.firebaseapp.com",
+    projectId: "nyous-4c48f",
+    storageBucket: "nyous-4c48f.appspot.com",
+    messagingSenderId: "680304415949",
+    appId: "1:680304415949:web:0e1952b1025de7688fc584"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebaseConfig;
